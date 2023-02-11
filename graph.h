3779d43ee670c705f3f2f6c040fd508ec76d23a8
kudr.max <mdkudryashov@edu.hse.ru>
2023-02-11 21:18:34 +0300

    Understood task of the lab properly and reoorganized elemets of classes

--- graph.h
+++ graph.h
@@ -2,20 +2,11 @@
 #include <unordered_map>
 
 namespace graph{
-    template <typename T1, typename T2, typename T3>
+    template <typename key_type, typename value_type, typename weight_type>
     class Graph{
     public:
-        // ПСЕВДОНИМЫ
-        using key_type = T1; // псевдоним для ключа
-        using value_type = T2; // псевдоним для значения
-        using weight_type = T3; // псевдоним для веса
+        Graph() = default;
 
-        // КОНСТРУКТОРЫ
-        Graph() noexcept = default;
-        Graph(const Graph&);// конструктор копирования
-        // консутрутор перемещения
-
-        // МЕТОДЫ
         bool empty() const{};
         size_t size() const{};
         void clear (){};
@@ -25,51 +16,25 @@ namespace graph{
         //size_t degree_out(key);
         //bool loop(key);
     private:
-        // РЕСУРСЫ
-        key_type m_key; // ключ
         class Node;
         std::unordered_map<key_type, Node> m_map; // мап из нод, которые содержит граф
     };
 }
 
-template <typename T1, typename T2, typename T3>
-class graph::Graph<T1, T2, T3>::Node{
+template <typename key_type, typename value_type, typename weight_type>
+class graph::Graph<key_type, value_type, weight_type>::Node{
 public:
-    // КОНСТРУКТОРЫ
-    Node() noexcept = default; // конструктор по умолчанию
+    Node() = default; // конструктор по умолчанию
     Node(value_type value); // конструктор с параметром
-    Node(const Node& node); // конструктор копирования
-    // консутрутор перемещения
 
-    // МЕТОДЫ
     bool empty(){};
     size_t size(){};
     value_type& value(){ return &m_value; }
     void clear (){};
 private:
-
-    // РЕСУРСЫ
     value_type m_value; // значение ноды
-    weight_type m_weight; // вес ноды
     std::unordered_map<key_type, weight_type> m_edge; // ребро, имеющее вес
 };
 
-// NODE
-
-template <typename T1, typename T2, typename T3> // конструктор с параметром
-graph::Graph<T1, T2, T3>::Node::Node(value_type value) : m_value{value} {};
-
-template <typename T1, typename T2, typename T3> // конструктор копирования
-graph::Graph<T1, T2, T3>::Node::Node(const Node& node) {
-    m_value = node.m_value;
-    m_weight = node.m_weight;
-    m_edge = node.m_edge;
-};
-
-// GRAPH
-
-template <typename T1, typename T2, typename T3> // конструктор копирования
-graph::Graph<T1, T2, T3>::Graph(const Graph& gr) {
-    m_key = gr.m_key;
-    m_map = gr.m_map;
-};
+template <typename key_type, typename value_type, typename weight_type> // конструктор с параметром
+graph::Graph<key_type, value_type, weight_type>::Node::Node(value_type value) : m_value{value} {};
