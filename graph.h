6586ef677493bea6253ca259b3e75d01f6db1c60
kudr.max <mdkudryashov@edu.hse.ru>
2023-02-12 10:23:45 +0300

    Created  working method value() for Node class

--- graph.h
+++ graph.h
@@ -1,40 +1,53 @@
 #pragma once
-#include <unordered_map>
 
-namespace graph{
-    template <typename key_type, typename value_type, typename weight_type>
-    class Graph{
+#include <unordered_map>
+#include <iostream>
+
+namespace graph {
+    template<typename key_type, typename value_type, typename weight_type>
+    class Graph {
     public:
+        class Node;
+
         Graph() = default;
 
-        bool empty() const{};
-        size_t size() const{};
-        void clear (){};
-        void swap(){};
+        bool empty() const {};
+
+        size_t size() const {};
+
+        void clear() {};
+
+        void swap() {};
         //Node& at(key);
         //size_t degree_in(key);
         //size_t degree_out(key);
         //bool loop(key);
     private:
-        class Node;
         std::unordered_map<key_type, Node> m_map; // мап из нод, которые содержит граф
     };
 }
 
-template <typename key_type, typename value_type, typename weight_type>
-class graph::Graph<key_type, value_type, weight_type>::Node{
+template<typename key_type, typename value_type, typename weight_type>
+class graph::Graph<key_type, value_type, weight_type>::Node {
 public:
     Node() = default; // конструктор по умолчанию
-    Node(value_type value); // конструктор с параметром
+    Node(value_type value) : m_value(value) {}; // конструктор с параметром
 
-    bool empty(){};
-    size_t size(){};
-    value_type& value(){ return &m_value; }
-    void clear (){};
+    bool empty();
+
+    size_t size();
+
+    value_type &value() { return m_value; }
+
+    void clear();
+
+    void print() {
+        std::cout << m_value << std::endl;
+        for (auto const &pair: m_edge)
+            std::cout << "{" << pair.first << ": " << pair.second << "}\n";
+    };
+
 private:
     value_type m_value; // значение ноды
     std::unordered_map<key_type, weight_type> m_edge; // ребро, имеющее вес
 };
-
-template <typename key_type, typename value_type, typename weight_type> // конструктор с параметром
-graph::Graph<key_type, value_type, weight_type>::Node::Node(value_type value) : m_value{value} {};
--- main.cpp
+++ main.cpp
@@ -2,6 +2,14 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    graph::Graph<int, int, int>::Node node1;
+    graph::Graph<int, int, int>::Node node2(6);
+    //int val = 5;
+    //node1.value() = val;
+    node2.print();
+    node2.value() = 5;
+    node1.value() = 10;
+    std::cout << node2.value() << " " << node1.value();
+    //std::cout << "Hello, World!" << node2.value() << std::endl;
     return 0;
 }
