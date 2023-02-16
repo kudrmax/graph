cfc2b981f2bc4c22df262afe2ed0ff1d50b7db9e
kudr.max <mdkudryashov@edu.hse.ru>
2023-02-16 15:01:36 +0300

    Added inerator and const_iterator for Graph class

--- graph.h
+++ graph.h
@@ -8,41 +8,57 @@ namespace graph {
     class Graph {
     public:
         class Node;
+        using const_iterator = typename std::unordered_map<key_type, Node>::const_iterator;
+        using iterator = typename std::unordered_map<key_type, Node>::iterator;
+
+        const_iterator cbegin() const { return m_map.cbegin(); };
+        const_iterator cend() const { return m_map.cend(); };
+        iterator begin() { return m_map.begin(); };
+        iterator end() { return m_map.end(); };
 
         Graph() = default;
+        Graph(key_type key, value_type value, weight_type weight) {
+            Node temp(key, value, weight);
+            m_map.emplace(key, temp);
+        };
 
         bool empty() const {};
-
         size_t size() const {};
-
         void clear() {};
-
         void swap() {};
+
         //Node& at(key);
         //size_t degree_in(key);
         //size_t degree_out(key);
         //bool loop(key);
-    private:
+        void print() const;
+
+//        std::pair<Graph::iterator, bool> insert_node(key_type key, value_type val);
+    public:
         std::unordered_map<key_type, Node> m_map; // мап из нод, которые содержит граф
     };
 }
+
 
 template<typename key_type, typename value_type, typename weight_type>
 class graph::Graph<key_type, value_type, weight_type>::Node {
 public:
     Node() = default; // конструктор по умолчанию
     Node(value_type value) : m_value(value) {}; // конструктор с параметром
+    Node(key_type key, value_type value, weight_type weight) : m_value(value) {
+        m_edge.emplace(key, weight);
+    };
+
 
     bool empty() const;
-
     size_t size() const;
 
-    value_type &value() { return m_value; }
+    value_type& value() { return m_value; }
 
     void clear();
-
     void print() const;
 
+//    void swap(Node& obj1, Node& obj2);
 private:
     value_type m_value; // значение ноды
     std::unordered_map<key_type, weight_type> m_edge; // ребро, имеющее вес
@@ -63,12 +79,35 @@ void graph::Graph<key_type, value_type, weight_type>::Node::clear() {
     m_edge.clear();
 }
 
+//template<typename key_type, typename value_type, typename weight_type>
+//void graph::swap(Node& obj1, Node& obj2) {
+//    Node temp = obj1;
+//    obj1 = obj2;
+//    obj2 = temp;
+//}
+
 template<typename key_type, typename value_type, typename weight_type>
 void graph::Graph<key_type, value_type, weight_type>::Node::print() const {
     std::cout << "m_value: " << m_value << std::endl;
-    for (auto const &pair: m_edge)
+    for (auto const& pair: m_edge)
         std::cout << "m_edge: {" << pair.first << ": " << pair.second << "}" << std::endl;
     std::cout << "empty(): " << empty() << std::endl;
     std::cout << "size(): " << size() << std::endl;
-
+    std::cout << std::endl;
 };
+
+template<typename key_type, typename value_type, typename weight_type>
+void graph::Graph<key_type, value_type, weight_type>::print() const {
+    std::cout << "I'm here";
+    for (auto const& node: m_map) {
+        std::cout << node.first << std::endl;
+        (node.second).print();
+    }
+};
+
+//template<typename key_type, typename value_type, typename weight_type>
+//std::pair<graph::Graph<key_type, value_type, weight_type>::iterator, bool>
+//graph::Graph<key_type, value_type, weight_type>::insert_node(key_type key, value_type val) {
+//
+//    return std::make_pair(it, 1);
+//}
--- main.cpp
+++ main.cpp
@@ -3,12 +3,32 @@
 #include <unordered_map>
 
 int main() {
-    graph::Graph<int, int, int>::Node node(6);
-    node.print();
-    node.clear();
 
-    //std::unordered_map<int, int> some_map;
-    //std::cout << some_map.empty() << std::endl;
+    graph::Graph<int, int, int> gr1(11, 22, 33);
+    gr1.begin()->second;
 
+//    gr1.begin()->print();
+//        gr1.m_map.begin();
+//    node1.print();
+//    node2.print();
+
+//    graph::swap(node1, node2);
+//    gr1.print();
+
+
+
+//    graph::Graph<int, int, int> gr;
+//    gr.print();
+
+//    std::unordered_map<int, int> some_map;
+//    some_map.emplace(1, 11);
+//    some_map.emplace(4, 44);
+//    some_map.emplace(3, 33);
+//
+//    for (auto const& pair: some_map)
+//        std::cout << "{" << pair.first << ": " << pair.second << "}" << std::endl;
+//    std::cout << "{" << some_map.begin()->first << ": " << some_map.begin()->second << "}" << std::endl;
+//    std::cout << "{" << some_map.begin().operator++()->first << ": " << some_map.begin().operator++()->second << "}"
+//              << std::endl;
     return 0;
 }
